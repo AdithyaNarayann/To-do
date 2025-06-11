2d98dc13742bef7259eb47a57808b91d9ebe4105
Adithya Narayan <adithyanarayankannan@gmail.com>
2025-06-12 00:04:50 +0530

    push

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const listoftasks = document.getElementById("bottom")
+const tasksec = document.getElementById("bottom")
+const listoftasks = document.createElement("ul")
+tasksec.append(listoftasks)
+listoftasks.className = "listoftasks"
+
+function newtask(){
+    const input = document.getElementById("theinput")
+    const task = document.createElement("li")
+    task.className = "oneofthetasks"
+    task.textContent = input.value
+    listoftasks.append(task)
+    input.value=""
+    input.focus()
+}
