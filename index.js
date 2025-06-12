7a22c646027d97ab3b853f56f6635fcaf0fc9d88
Adithya Narayan <adithyanarayankannan@gmail.com>
2025-06-12 11:50:48 +0530

    push

--- index.js
+++ index.js
@@ -9,31 +9,36 @@ function newtask(){
     const input = document.getElementById("theinput")
     const task = document.createElement("li")
     task.className = "oneofthetasks"
-    const createbutton = document.createElement("button")
     const deletebutton = document.createElement("button")
-    createbutton.textContent = "create"
     deletebutton.textContent = "delete"
     const circle = document.createElement("span")
     circle.className = "circle"
     const text = document.createElement("span")
     text.className = "text"
+    if(!input.value.trim()){
+        input.value = ""
+        input.focus()
+        return
+    }
     text.textContent = input.value
     const bottomright = document.createElement("div")
     bottomright.className = "bottomright"
     const bottomleft = document.createElement("div")
     bottomleft.className = "bottomleft"
     bottomleft.append(circle,text)
-    bottomright.append(createbutton,deletebutton)
+    bottomright.append(deletebutton)
     task.append(bottomleft,bottomright)
     listoftasks.append(task)
     circle.addEventListener("click",()=>{
         circle.classList.toggle("checked");
         console.log(task.classList);
     })
+    deletebutton.addEventListener("click",()=>{
+        task.remove()
+    })
     input.value=""
     input.focus()
 }
 
 bottommain.append(listoftasks)
-
 
