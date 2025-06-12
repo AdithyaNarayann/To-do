92c5378cf99ac3befa66e99dafc596d5f23dd344
Adithya Narayan <adithyanarayankannan@gmail.com>
2025-06-12 11:23:24 +0530

    added toggle

--- index.js
+++ index.js
@@ -3,15 +3,37 @@ const bottommain = document.createElement("div")
 bottommain.className = "bottom-main"
 tasksec.append(bottommain)
 const listoftasks = document.createElement("ul")
-bottommain.append(listoftasks)
 listoftasks.className = "listoftasks"
 
 function newtask(){
     const input = document.getElementById("theinput")
     const task = document.createElement("li")
     task.className = "oneofthetasks"
-    task.textContent = input.value
+    const createbutton = document.createElement("button")
+    const deletebutton = document.createElement("button")
+    createbutton.textContent = "create"
+    deletebutton.textContent = "delete"
+    const circle = document.createElement("span")
+    circle.className = "circle"
+    const text = document.createElement("span")
+    text.className = "text"
+    text.textContent = input.value
+    const bottomright = document.createElement("div")
+    bottomright.className = "bottomright"
+    const bottomleft = document.createElement("div")
+    bottomleft.className = "bottomleft"
+    bottomleft.append(circle,text)
+    bottomright.append(createbutton,deletebutton)
+    task.append(bottomleft,bottomright)
     listoftasks.append(task)
+    circle.addEventListener("click",()=>{
+        circle.classList.toggle("checked");
+        console.log(task.classList);
+    })
     input.value=""
     input.focus()
 }
+
+bottommain.append(listoftasks)
+
+
